470dcf9eb1936464a2d8b73762e8bf14a3eaf5a5
dngiang <dongocgiang.edu@gmail.com>
2024-04-08 22:14:20 +0700

    🤘 Init api ping service (#3)

--- src/server.js
+++ src/server.js
@@ -1,1 +1,17 @@
-console.log('Hello from server.js');
+const express = require('express');
+const httpStatus = require('http-status');
+
+const { env } = require('./config');
+
+const app = express();
+
+app.get('/ping', (req, res) => {
+  res.send({
+    code: httpStatus.OK,
+    message: 'Service mailer is running',
+  });
+});
+
+app.listen(env.port, () => {
+  console.log(`Service running on port ${env.port}`);
+});
