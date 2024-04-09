e80b194998e6b9250352e54b0f29ddef11842c90
dngiang <dongocgiang.edu@gmail.com>
2024-04-09 22:54:29 +0700

    🤙 Config base email transport (#5)

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -2,6 +2,17 @@ require('dotenv').config();
 
 const env = {
   port: process.env.PORT || 3000,
+  email: {
+    smtp: {
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      auth: {
+        user: process.env.SMTP_USERNAME,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    },
+    from: process.env.SMTP_USERNAME,
+  },
 };
 
 module.exports = env;
